954d8ba788de284e163a665395efc698d29340b2
5a9awneh <105069363+5a9awneh@users.noreply.github.com>
2023-11-30 15:22:32 +0300

    Improve Git Log Parsing to Exclude Merge Commits and Handle Variations
    This update enhances the script's ability to parse Git commit logs more accurately. Key improvements include:
    
    1. Exclusion of Merge Commits: Modified the Git log command to exclude merge commits by adding the `--no-merges` flag. This change streamlines the processing of commits by focusing only on regular commits that include change statistics.
    
    2. Robust Parsing: Refined the regular expression in the `parse_commit_log` function to more flexibly handle variations in commit change statistics. The script can now accurately capture and process different formats of file change information (files changed, insertions, deletions), ensuring completeness and accuracy in the CSV output.
    
    These enhancements ensure that the script functions effectively across a wider range of Git repositories, providing reliable and comprehensive parsing of commit logs.

--- main.py
+++ main.py
@@ -12,17 +12,6 @@ def get_repo_name(repo_dir):
     return repo_name
 
 
-def get_current_git_username(repo_dir):
-    result = subprocess.run(
-        ["git", "-C", repo_dir, "config", "user.name"], capture_output=True, text=True
-    )
-    if result.returncode != 0:
-        print("Error fetching current Git username:", result.stderr)
-        return None
-
-    return result.stdout.strip()
-
-
 def generate_commit_log(repo_dir, author, output_file_name):
     git_command = [
         "git",
@@ -30,6 +19,7 @@ def generate_commit_log(repo_dir, author, output_file_name):
         repo_dir,
         "log",
         "--all",
+        "--no-merges",  # Exclude merge commits
         f"--author={author}",
         "--reverse",
         "--shortstat",
@@ -52,45 +42,56 @@ def parse_commit_log(file_path):
     parsed_commits = []
 
     for block in commit_blocks:
-        pattern = r"^([\da-f]+)\nAuthor: (.+?) <.+?>\nDate: (.+?)\n\n(.+?)\n\n (\d+) files? changed, (\d+) insertions?\(\+\), (\d+) deletions?\(-\)"
-        match = re.search(pattern, block, re.DOTALL)
+        parts = block.split("\n\n", 2)
+        if len(parts) < 3:
+            continue
 
-        if match:
-            (
-                commit_id,
-                author,
-                date_time_str,
-                message,
-                files_changed,
-                insertions,
-                deletions,
-            ) = match.groups()
-            date_time_str = date_time_str.strip()
+        header, message, change_stats = parts
+        commit_id, author, author_email, date_time_str = re.match(
+            r"^([\da-f]+)\nAuthor: (.+?) <(.+?)>\nDate: (.+?)$", header, re.DOTALL
+        ).groups()
 
-            try:
-                date_time_obj = datetime.strptime(
-                    date_time_str, "%a %b %d %H:%M:%S %Y %z"
-                )
-                date = date_time_obj.strftime("%d/%m/%Y")
-                time = date_time_obj.strftime("%H:%M:%S %z")
-            except ValueError as e:
-                print(f"Error parsing date and time: {e}")
-                date = "Invalid Date"
-                time = "Invalid Time"
+        # Process change stats more flexibly
+        files_changed = insertions = deletions = "0"
+        stats_pattern = r"(\d+) files? changed"
+        insertions_pattern = r"(\d+) insertions?\(\+\)"
+        deletions_pattern = r"(\d+) deletions?\(-\)"
 
-            clean_message = message.strip()
-            parsed_commits.append(
-                {
-                    "Commit ID": commit_id,
-                    "Author": author,
-                    "Date": date,
-                    "Time": time,
-                    "Message": clean_message,
-                    "Files Changed": files_changed,
-                    "Insertions (+)": insertions,
-                    "Deletions (-)": deletions,
-                }
-            )
+        stats_match = re.search(stats_pattern, change_stats)
+        insertions_match = re.search(insertions_pattern, change_stats)
+        deletions_match = re.search(deletions_pattern, change_stats)
+
+        if stats_match:
+            files_changed = stats_match.group(1)
+        if insertions_match:
+            insertions = insertions_match.group(1)
+        if deletions_match:
+            deletions = deletions_match.group(1)
+
+        # Date and time parsing
+        date_time_str = date_time_str.strip()
+        try:
+            date_time_obj = datetime.strptime(date_time_str, "%a %b %d %H:%M:%S %Y %z")
+            date = date_time_obj.strftime("%d/%m/%Y")
+            time = date_time_obj.strftime("%H:%M:%S %z")
+        except ValueError as e:
+            print(f"Error parsing date and time for commit {commit_id}: {e}")
+            date = "Invalid Date"
+            time = "Invalid Time"
+
+        clean_message = message.strip()
+        parsed_commits.append(
+            {
+                "Commit ID": commit_id,
+                "Author": author,
+                "Date": date,
+                "Time": time,
+                "Message": clean_message,
+                "Files Changed": files_changed,
+                "Insertions (+)": insertions,
+                "Deletions (-)": deletions,
+            }
+        )
 
     return pd.DataFrame(parsed_commits)
 
@@ -102,26 +103,19 @@ def main():
         return
 
     repo_name = get_repo_name(repo_dir)
-    author_username = get_current_git_username(repo_dir)
-    if author_username:
-        git_output_file = f"{repo_name}_commit_history.txt"
-        csv_output_file = f"{repo_name}_commit_history.csv"
+    author_username = "5a9awneh"
 
-        commit_log_file = generate_commit_log(
-            repo_dir, author_username, git_output_file
-        )
-        if commit_log_file:
-            df = parse_commit_log(commit_log_file)
-            df.to_csv(csv_output_file, index=False, quoting=csv.QUOTE_NONNUMERIC)
-            print(f"CSV file created: {csv_output_file}")
+    git_output_file = f"{repo_name}_commit_history.txt"
+    csv_output_file = f"{repo_name}_commit_history.csv"
 
-            # Delete the txt file after successful CSV generation
-            os.remove(commit_log_file)
-            print(f"Deleted temporary file: {git_output_file}")
-    else:
-        print(
-            "Failed to identify the current Git user. Ensure you are in a Git repository directory."
-        )
+    commit_log_file = generate_commit_log(repo_dir, author_username, git_output_file)
+    if commit_log_file:
+        df = parse_commit_log(commit_log_file)
+        df.to_csv(csv_output_file, index=False, quoting=csv.QUOTE_NONNUMERIC)
+        print(f"CSV file created: {csv_output_file}")
+
+        os.remove(commit_log_file)
+        print(f"Deleted temporary file: {git_output_file}")
 
 
 if __name__ == "__main__":
